150334dec7da66bd3d8538736710bb2504db9db2
Anh Pati <romain-felicianne@hotmail.fr>
2019-01-06 22:23:25 +0100

    Abandon (provisoire ?) du sticky menu. Finalisation du style du menu. Début du responsive du menu.

--- js/sticky-menu.js
+++ js/sticky-menu.js
@@ -1,10 +1,10 @@
-let header = document.querySelector("#atgHeader");
+let header = document.querySelector("#atgHeader header");
 
-function scrolled() {
-    let windowHeigth = document.body.clientHeight;
-    let currentScroll = document.body.scrollTop || document.documentElement.scrollTop;
-
-    header.className = (currentScroll >= windowHeight - header.offsetHeight) ? "fixed" : "";
+function scrolled(){
+	let windowHeight = document.body.clientHeight,
+		currentScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	
+	header.className = (currentScroll >= windowHeight - header.offsetHeight) ? "fixed" : "";
 }
 
 addEventListener("scroll", scrolled, false);
